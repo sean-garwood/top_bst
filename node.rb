2d452b6f44925c4a468b8a8ad8821506b8a99292
sean-garwood <seansgarwood@gmaill.com>
2023-12-24 14:10:21 -0500

    Add Comparable

--- node.rb
+++ node.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+#bonus: compare nodes using data.
+module Comparable
+  def same_data?(a, b)
+    a.data == b.data
+  end
+
+  def less_than?(a, b)
+    a.data < b.data
+  end
+end
+
 # represents a node on a binary search tree
 class Node
   def initialize(value = nil, left = nil, right = nil)
