7beac0d0245f267ef026f88456d0ed0ab8fb1510
sean-garwood <seansgarwood@gmaill.com>
2023-12-26 09:47:08 -0500

    Define build_tree

--- node.rb
+++ node.rb
@@ -2,12 +2,12 @@
 
 # bonus: compare nodes using data.
 module Comparable
-  def same_data?(a, b)
-    a.data == b.data
+  def same_data?(node_a, node_b)
+    node_a.data == node_b.data
   end
 
-  def less_than?(a, b)
-    a.data < b.data
+  def less_than?(node_a, node_b)
+    node_a.data < node_b.data
   end
 end
 
@@ -31,5 +31,8 @@ class Tree
 
   def build_tree(arr)
     arr = arr.uniq.sort
+    arr.each { |e| Node.new(e) }
+    mid = arr.length / 2
+    mid.floor
   end
 end
