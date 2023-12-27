0701647d4edc4b2d5e93289adc527928ff9f0607
sean-garwood <seansgarwood@gmaill.com>
2023-12-27 13:34:23 -0500

    Define helper methods, set @root
    find_middle_index, one_left, one_right all return ints that represent
    the indices of the 'root' element, which is the middle element in the
    sorted array, as well as its left and right neighbors.
    
    Thinking of storing these in a three-element array and using this as a
    recursive way to build the tree, where:
    
    L0: root = mid element, left = left of root_l0, right = right_of_root_l0
    L1: two trees: left and right
      tree one:
    
      t1_l1 root: left_l0
      t1_l1 left = left of root_t1_l1
      t1_l1 right = root_l0
    
      t1_r1 root = right_l0
      t1_r1 left = root_l0
      t1_r1 right = right of root_t2_l1

--- node.rb
+++ node.rb
@@ -21,15 +21,21 @@ class Tree
   def initialize(arr = nil)
     @arr = arr
     # need to find the middle of the sorted arr to set the root index
-    @root = find_middle_index
+    @root = root_of_tree(arr)
+  end
+
+  def clean(arr)
+    return nil if array.nil?
+
+    arr.uniq.sort
   end
 
   def find_middle_index(arr)
-    return nil if array.nil?
+    clean(arr).(length / 2).floor
+  end
 
-    arr = arr.uniq.sort
-    mid = arr.length / 2
-    mid.floor
+  def root_of_tree(arr)
+    Node.new(arr[find_middle_index(arr)])
   end
 
   def one_left(index)
@@ -39,4 +45,36 @@ class Tree
   def one_right(index)
     index + 1
   end
+
+  def halves(arr)
+    # remove root, left and right from arr, return halves
+    mid = find_middle_index(arr)
+    left = one_left(mid)
+    right = one_right(mid)
+    [arr[0..left - 1], arr[right + 1..]]
+  end
+
+  def build_base(root, left, right)
+    # need to recursively do the following:
+      # 1. clean array input
+      # 2. find middle index of array
+      # 3. create a Tree instance with
+  end
+
+  def build_tree(three_element_arr)
+    three_element_arr.each { |e| Node.new(e)}
 end
+
+# high-level
+
+# given an arbitrary array, build a balanced binary search tree consisting of
+# its elements, where the middle element is the root, and all the elements to
+# the left of root are less than root, and those to the right are greater than
+# root.
+
+# take array, check if nil
+# remove duplicates
+# sort array, return
+
+# get middle element
+# set as root of Tree
