6b54b1f4bf00b24dd353e8cb1b6e4881e50adcf0
sean-garwood <seansgarwood@gmaill.com>
2023-12-27 13:03:50 -0500

    Formatting

--- pretty_print.rb
+++ pretty_print.rb
@@ -2,8 +2,9 @@
 
 # visual display of BST
 module PrettyPrint
-def pretty_print(node = @root, prefix = '', is_left: true)
-  pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
-  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-  pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  def pretty_print(node = @root, prefix = '', is_left: true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
