774fc1ed2a8976fb6bc1c82a8451a0782a026b2a
sean-garwood <seansgarwood@gmaill.com>
2023-12-27 12:45:15 -0500

    Initial commit

---
+++ pretty_print.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# visual display of BST
+module PrettyPrint
+def pretty_print(node = @root, prefix = '', is_left: true)
+  pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+  puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+  pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+end
