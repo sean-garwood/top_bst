13f6ede58418d9e1397453979dce9a7af0f03c77
sean-garwood <seansgarwood@gmaill.com>
2023-12-30 13:33:18 -0500

    Finish #insert, refactor to #find_parent

--- tree.rb
+++ tree.rb
@@ -17,22 +17,17 @@ class Tree
   end
 
   def insert(data)
+    return @root = Node.new(data) if @root.nil?
     return if same?(data, @root.data)
 
     new_node = Node.new(data)
     # store the last node that was compared so its left/right attrib can be
     #    modified when the proper placement is found for the new node
-    last = @root
-    current = last
-    # proc to send different nodes to compare to a block that returns the next
-    # node: left if
-    next_node = proc do
-      less_than?(current, new_node) ? current.left : current.right
-    end
-    # need to set last.left or right depending on last path taken
-
-    end
-    # what to do after we find neighbors?
+    # need to reset the prev. node's left or right attr
+    #   if last < new, last.left = new
+    #   if last > new, last.right = new
+    parent = find_parent
+    parent <=> new_node.negative? parent.left = new_node : parent.right = new_node
   end
 
 
@@ -49,4 +44,21 @@ class Tree
     root_node.right = build_tree(arr[mid + 1..])
     root_node
   end
+
+  def find_parent
+    parent = @root
+    current = last
+    # proc to send different nodes to compare to a block that returns the next
+    # node: left if
+    next_node = proc do
+      less_than?(current, new_node) ? current.left : current.right
+    end
+    # compare the current node and the new_node
+    # update next_node and last until the following conditions are met:
+    #   new_node > last
+    #   new_node < next
+    until greater_than?(new_node, parent) && less_than?(new_node, next_node)
+      parent = current
+      current = next_node.call
+    end
 end
