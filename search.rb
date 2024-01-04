a4a51481fc6e0dd9cf1e8805be986ddb7adfc915
sean-garwood <seansgarwood@gmaill.com>
2024-01-03 22:17:49 -0500

    Define #find

--- search.rb
+++ search.rb
@@ -2,6 +2,12 @@
 
 # Search for nodes in the BST.
 module Search
+  def direction(node, current)
+    return if current.leaf?
+
+    less_than?(node, current) || current.right.nil? ? -1 : 1
+  end
+
   def find_parent(child, current)
     until current.leaf?
       dir = direction(child, current)
@@ -10,9 +16,15 @@ module Search
     current
   end
 
-  def direction(node, current)
-    return if current.leaf?
+  def find(value)
+    return if value.nil? || empty?
 
-    less_than?(node, current) || current.right.nil? ? -1 : 1
+    node = Node.new(value)
+    current = @root
+    until same?(node, current) || current.leaf?
+      dir = direction(node, current)
+      dir.negative? && current = current.left || current = current.right
+    end
+    current
   end
 end
