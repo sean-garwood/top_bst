b923ebb27773720530ab290f4419c2cd4c59eed0
sean-garwood <seansgarwood@gmaill.com>
2024-01-08 21:51:28 -0500

    Sure whatever

--- main.rb
+++ main.rb
@@ -3,9 +3,7 @@
 require_relative 'node'
 require_relative 'tree'
 
-foo = Array.new(4) { rand(1..100) }
+foo = Array.new(12) { rand(1..100) }
 bar = Tree.new(foo)
 
-puts "initial tree\n#{bar.pretty_print}\n\n"
-bar.insert(-1)
-puts "modified tree\n#{bar.pretty_print}\n\n"
+puts "#{bar.pretty_print}\n\n"
