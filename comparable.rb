65856e988bf1c00e6d1c5a13a7f670ba1b5df179
sean-garwood <seansgarwood@gmaill.com>
2024-01-08 21:50:50 -0500

    Fix dumb stuff

--- comparable.rb
+++ comparable.rb
@@ -3,14 +3,20 @@
 # bonus: compare nodes using data.
 module Comparable
   def same?(node)
-    @data == node.data
+    return if node.nil? && !@data.nil?
+
+    @data == node.data || @data.nil? && node.data.nil?
   end
 
   def less_than?(node)
-    @data < node.data unless same?(node)
+    return if same?(node) || @data.nil?
+
+    node.data.nil? || node.data < @data
   end
 
   def greater_than?(node)
-    !less_than?(node) unless same?(node)
+    return if @data.nil? || same?(node)
+
+    !less_than?(node)
   end
 end
