3bab4cc44c421bc3a189ffe199ec5ab15a45dcbd
sean-garwood <seansgarwood@gmaill.com>
2024-01-10 13:45:24 -0500

    Add driver back

---
+++ main.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require_relative 'lib/comparable'
+require_relative 'lib/search'
+
+require_relative 'lib/node'
+require_relative 'lib/tree'
+
+module TestArrays
+  BASIC_ARR = [1, 2, 3, 4].freeze
+  LONG_BASIC = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
+  REALLY_LONG = Array.new(64) { rand(1..100) }.freeze
+end
+
+foo = Tree.new(TestArrays::BASIC_ARR)
+bar = Tree.new(TestArrays::LONG_BASIC)
+baz = Tree.new(TestArrays::REALLY_LONG)
+
+foo.insert(100)
+
+foo.pretty_print
+
+puts "here it is: 100! -=-#{foo.find(100).data}"
+
+bar.delete(1)
+bar.pretty_print
