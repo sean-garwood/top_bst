a7ce0d34ef5b567dfdb557fa63a31fadcaca6443
sean-garwood <seansgarwood@gmaill.com>
2024-01-09 21:25:59 -0500

    More dummies

--- main.rb
+++ main.rb
@@ -4,9 +4,9 @@ require_relative 'node'
 require_relative 'tree'
 
 module TestArrays
-  BASIC_ARR = [1, 2, 3, 4]
-  LONG_BASIC = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-  REALLY_LONG = Array.new(64) { rand(1..100) }
+  BASIC_ARR = [1, 2, 3, 4].freeze
+  LONG_BASIC = [1, 2, 3, 4, 5, 6, 7, 8, 9].freeze
+  REALLY_LONG = Array.new(64) { rand(1..100) }.freeze
 end
 
 foo = Tree.new(TestArrays::BASIC_ARR)
@@ -14,6 +14,6 @@ bar = Tree.new(TestArrays::LONG_BASIC)
 baz = Tree.new(TestArrays::REALLY_LONG)
 
 foo.pretty_print
-foo.insert(-1)
+foo.insert(1)
 
 foo.pretty_print
