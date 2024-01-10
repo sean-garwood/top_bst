dd82f0bf5bf98691720bda3d5a7a712e70ce809b
sean-garwood <seansgarwood@gmaill.com>
2024-01-10 17:02:36 -0500

    Initial commit

---
+++ lib/pretty_print.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# print trees and subtrees gracefully to the console.
+# i stole this mod.
+module PrettyPrintz
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+end
